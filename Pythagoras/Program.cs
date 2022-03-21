7e87de6054ee50afa3d477e0f8900bcf4638d7c1
Frederik Hvid Sørensen <fred09j3@zbc.dk>
2022-03-21 13:24:30 +0100

    Solved for c

--- Pythagoras/Program.cs
+++ Pythagoras/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input;
+double a, aPow2, b, bPow2, c;
+
+// Get input for a
+do
+{
+    Console.WriteLine("Input a²: ");
+    input = Console.ReadLine();
+}
+while (!double.TryParse(input, out aPow2));
+
+// Get input for b
+do
+{
+    Console.WriteLine("Input b²: ");
+    input = Console.ReadLine();
+}
+while (!double.TryParse(input, out bPow2));
+
+a = Math.Sqrt(aPow2);
+b = Math.Sqrt(bPow2);
+
+c = a + b;
+
+Console.WriteLine($"a² + b² = c²\nc = {c}");
+
+if (a > b)
+{
+    Console.WriteLine($"a was bigger. ({a} > {b})");
+}
+else
+{
+    Console.WriteLine($"b was bigger. ({b} > {a})");
+}
